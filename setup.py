d4656f695a3089fcd85589cb394d0a9acbdd9942
ysak-y <yoshiaki.0614@gmail.com>
2017-01-21 23:37:29 +0900

    Move files for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='lexi',
+version='0.1',
+description='Easy development tool for Lex',
+url='https://github.com/ysak-y/lexi-test.git',
+author='Yoshiaki Yamada',
+author_email='yoshiaki.0614@gmail.com',
+license='MIT',
+packages=['lexi'],
+zip_safe=False)
+

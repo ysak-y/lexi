3a553294c0b75db6932a1904180773aa3f2a2891
ysak-y <yoshiaki.0614@gmail.com>
2017-01-23 00:02:02 +0900

    Add basic responses

--- lexi/lex_io.py
+++ lexi/lex_io.py
@@ -70,19 +70,174 @@ Response sample
 }
 """
 
-DEFAULT_RESPONSE = {
-            'dialogAction': {
-                    'type': 'ElicitIntent'
+class Response(object):
+
+    def __init__(self, dic):
+        self.content = dic
+
+    def with_session_attributes(self, session_attributes):
+        self.content['sessionAttributes'] = session_attributes
+        return self
+
+class ResponseBuilder(object):
+
+    @classmethod
+    def elicit_intent(self, message=None, card=None):
+        '''
+            Argments
+
+            message:
+                Optional. you can build message using MessageBuilder.
+                If message is None, Amazon Lex uses one of the bot's
+                clarification prompts.(see the Error Handling section in the console.)
+
+            card:
+                Optional. you can build card using CardBuilder.
+                If card is None, Amazon Lex uses one of the bot's
+                clarification prompts.(see the Error Handling section in the console.)
+        '''
+        response = _build_response('ElicitSlot', message=message, card=card)
+        return Response(response)
+
+    @classmethod
+    def elicit_slot(self, intent_name, slot_to_elicit, slots, message=None, card=None):
+        '''
+            Argments
+
+            # Required
+
+            intent_name:
+                Basicaly, set requests['currentIntent']['name']
+
+            slots:
+                Slots must include all of the slots configured for the intent.
+                If the value of a slot is unknown, it must be explicitly set to null (similar to Lambda function request)
+
+            slot_to_elicit:
+                 Informs Amazon Lex that the user is expected to provide a slot value in response.
+                 For example, a value for the pizzaSize or pizzaKind slots.
+
+            message:
+                Optional. you can build message using MessageBuilder.
+                If message is None, Amazon Lex uses one of the bot's
+                clarification prompts.(see the Error Handling section in the console.)
+
+            card:
+                Optional. you can build card using CardBuilder.
+                If card is None, Amazon Lex uses one of the bot's
+                clarification prompts.(see the Error Handling section in the console.)
+        '''
+        response = _build_response('ElicitSlot', intent_name=intent_name, slot_to_elicit=slot_to_elicit, slots=slots, message=message, card=card)
+        return Response(response)
+
+    @classmethod
+    def confirm_intent(self, intent_name, slots, message=None, card=None):
+        '''
+            Argments
+
+            # Required
+
+            intent_name:
+                Basicaly, set requests['currentIntent']['name']
+
+            slots:
+                Slots must include all of the slots configured for the intent.
+                If the value of a slot is unknown, it must be explicitly set to null (similar to Lambda function request)
+
+            # Optional
+
+            message:
+                You can build message using MessageBuilder.
+                If message is None, Amazon Lex uses one of the bot's
+                clarification prompts.(see the Error Handling section in the console.)
+
+            card:
+                You can build card using CardBuilder.
+                If card is None, Amazon Lex uses one of the bot's
+                clarification prompts.(see the Error Handling section in the console.)
+        '''
+        response = _build_response('ConfirmIntent', intent_name=intent_name, slots=slots, message=message, card=card)
+        return Response(response)
+
+    @classmethod
+    def delegate(self, slots):
+        '''
+            Argments
+
+            # Required
+
+            slots:
+                Slots must include all of the slots configured for the intent.
+                If the value of a slot is unknown, it must be explicitly set to null (similar to Lambda function request)
+        '''
+        response = _build_response('delegate', slots=slots)
+        return Response(response)
+
+    @classmethod
+    def close(self, fulfillment_state, message=None, card=None):
+        '''
+            Argments
+
+            # Required
+
+            fulfillment_state:
+                Set 'Fulfilled' or 'Failed'.
+                Amazon Lex uses this value to set the dialogState in its response to the client.
+
+            # Optional
+
+            message:
+                You can build message using MessageBuilder.
+                If message is None, Amazon Lex uses one of the bot's
+                clarification prompts.(see the Error Handling section in the console.)
+
+            card:
+                You can build card using CardBuilder.
+                If card is None, Amazon Lex uses one of the bot's
+                clarification prompts.(see the Error Handling section in the console.)
+        '''
+        response = _build_response('close', fulfillment_state=fulfillment_state, message=message, card=card)
+        return Response(response)
+
+    def _build_response(self, response_type, fulfillment_state=None, message=None, intent_name=None, slots=None, slot_to_elicit=None, card=None):
+
+        dic = {
+                'dialogAction': {
+                        'type': response_type,
+                    }
                 }
-        }
 
-def lambda_handler(request_obj, context=None):
+        if fulfillment_state:
+            dic['dialogAction']['fulfillmentState'] = fulfillment_state
 
-    return DEFAULT_RESPONSE
+        if message:
+            dic['dialogAction']['message'] = message
 
+        if card:
+            dic['dialogAction']['responseCard'] = card
 
-class Request(object):
+        if intent_name:
+            dic['dialogAction']['intentName'] = intent_name
 
-    def __init__(self, request, metadata=None):
-       self.sessionAttributes = request["sessionAttributes"]
-       self.body = request
+        if slots:
+            dic['dialogAction']['slots'] = slots
+
+        if slot_to_elicit:
+            dic['dialogAction']['slotToElicit'] = slot_to_elicit
+
+        return dic
+
+class CardBuilder(object):
+
+    def __init__(self, version):
+        self.content = {}
+        self.content['version'] = version
+        self.content['contentType'] = 'application/vnd.amazonaws.card.generic'
+
+class MessageBuilder(object):
+
+    def __init__(self, content_type, content):
+        self.content = {
+                'contentType': content_type,
+                'content': content
+                }

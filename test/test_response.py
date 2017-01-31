5c98a6886ef8d00350ff83631cbf4dc1a05070ff
ysak-y <yoshiaki.0614@gmail.com>
2017-01-31 23:56:51 +0900

    t Rename tests to test for noses

--- test/test_response.py
+++ test/test_response.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath('..'))
 from lexi import lex_io as lex
 
 import unittest
-from nose.tools import *
+from nose2.tools import params
 from fixtures.responses import DEFAULT_RESPONSE
 from fixtures.requests import DEFAULT_REQUEST
 
@@ -14,9 +14,12 @@ class TestResponse(object):
     #def setUp(self):
 
     #def tearDown(self):
+    @params(1, 2, 3)
+    def test_nums(self, num):
+        assert num < 4
     
-    def test_default_response(self):
-        eq_(DEFAULT_RESPONSE, lex.lambda_handler(DEFAULT_REQUEST, None))
+#    def test_default_response(self):
+#        eq_(DEFAULT_RESPONSE, lex.lambda_handler(DEFAULT_REQUEST, None))
 
     #def test_elicit_slot_response(self):
 

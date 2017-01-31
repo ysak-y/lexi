371579a7ce414eaecfd3252f789ce3788f2bdb7c
ysak-y <yoshiaki.0614@gmail.com>
2017-02-01 00:22:51 +0900

    Update Response test

--- test/test_response.py
+++ test/test_response.py
@@ -3,20 +3,23 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath('..'))
-from lexi import lex_io as lex
+from lexi import lex
 
 import unittest
 from nose2.tools import params
 from fixtures.responses import DEFAULT_RESPONSE
 from fixtures.requests import DEFAULT_REQUEST
 
-class TestResponse(object):
+class TestResponse(unittest.TestCase):
+    def test_raise_elicit_slot_without_intent_name(self):
+        slots = ['2', '3', '4']
+        with self.assertRaises(TypeError):
+            lex.ResponseBuilder.elicit_slot(slots=slots)
+
+#class TestResponse(object):
     #def setUp(self):
 
     #def tearDown(self):
-    @params(1, 2, 3)
-    def test_nums(self, num):
-        assert num < 4
     
 #    def test_default_response(self):
 #        eq_(DEFAULT_RESPONSE, lex.lambda_handler(DEFAULT_REQUEST, None))

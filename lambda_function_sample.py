6a6ff945216f3d724b80bba0c2171dca71c0641a
ysak-y <yoshiaki.0614@gmail.com>
2017-02-01 23:27:40 +0900

    Add error handling in sample code

--- lambda_function_sample.py
+++ lambda_function_sample.py
@@ -5,4 +5,7 @@ from lexi import lex
 def lambda_handler(request, context=None):
     intent_name = request['currentIntent']['name']
     req = lex.Request(request).to_dict 
-    return globals()[intent_name](req, context).to_dict()
+    if intent_name in globals():
+        return globals()[intent_name](req, context).to_dict()
+    else:
+        return lex.ResponseBuilder.elicit_intent("Can't find intent name's function in lambda.")

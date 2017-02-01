fbc1a693434784ed3166c0fc1da86ab4d15baad9
ysak-y <yoshiaki.0614@gmail.com>
2017-02-01 23:32:09 +0900

    Add invalid test

--- test/test_response.py
+++ test/test_response.py
@@ -6,26 +6,41 @@ sys.path.insert(0, os.path.abspath('..'))
 from lexi import lex
 
 import unittest
-from nose2.tools import params
 from fixtures.responses import DEFAULT_RESPONSE
 from fixtures.requests import DEFAULT_REQUEST
 
 class TestResponse(unittest.TestCase):
+    '''
+        # Use setUp if common process before test.
+        def setUp(self):
+
+        # Use tearDown if common process after test.
+        def tearDown(self):
+    '''
     def test_raise_elicit_slot_without_intent_name(self):
-        slots = ['2', '3', '4']
+        slots = ['Cat', 'Dog']
         with self.assertRaises(TypeError):
             lex.ResponseBuilder.elicit_slot(slots=slots)
-
-#class TestResponse(object):
-    #def setUp(self):
-
-    #def tearDown(self):
     
-#    def test_default_response(self):
-#        eq_(DEFAULT_RESPONSE, lex.lambda_handler(DEFAULT_REQUEST, None))
+    def test_raise_elicit_slot_without_slots(self):
+        intent_name = 'AnimalIntent'
+        with self.assertRaises(TypeError):
+            lex.ResponseBuilder.elicit_slot(intent_name=intent_name)
 
-    #def test_elicit_slot_response(self):
+    def test_raise_confirm_intent_without_intent_name(self):
+        slots = ['Cat', 'Dog']
+        with self.assertRaises(TypeError):
+            lex.ResponseBuilder.confirm_intent(slots=slots)
 
-    #def test_confirm_intent_response(self):
+    def test_raise_confirm_intent_without_slots(self):
+        intent_name = 'AnimalIntent'
+        with self.assertRaises(TypeError):
+            lex.ResponseBuilder.confirm_intent(intent_name=intent_name)
 
-    #def test_
+    def test_raise_delegate_without_slots(self):
+        with self.assertRaises(TypeError):
+            lex.ResponseBuilder.delegate()
+
+    def test_raise_close_without_fulfillment_state(self):
+        with self.assertRaises(TypeError):
+            lex.ResponseBuilder.close()

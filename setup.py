e0c32d1efed7e3101d07b6ebd043437be3d9e2fd
yoshiaki-yamada <yoshiaki-yamada@cookpad.com>
2017-06-11 19:40:00 +0900

    Update package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='lexus',
-version='0.1',
+version='0.2',
 description='Easy development tool for AWS Lex',
 url='https://github.com/ysak-y/lexus-test.git',
 author='Yoshiaki Yamada',
